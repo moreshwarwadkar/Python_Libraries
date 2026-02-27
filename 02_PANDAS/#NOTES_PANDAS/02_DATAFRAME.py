e3d4f6b9782747457a4b47ec47e86c2ab5927d0b
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2026-02-27 16:56:57 +0530

    Create 02_DATAFRAME.py

---
+++ 02_PANDAS/#NOTES_PANDAS/02_DATAFRAME.py
@@ -0,0 +1,9 @@
+#----- ----- DATAFRAME ----- -----
+
+#- Collection of Rows and Columns. or
+#- Collection of Series.
+
+#----- ----- Way To Create ----- -----
+
+#- pd.DataFrame(DICTIONARY)
+#['D' and 'F' Should be Uppercase]
