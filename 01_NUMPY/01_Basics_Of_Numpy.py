b74275931b6a7e3870bf4783e4d6be7c007204d0
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2026-03-02 21:45:53 +0530

    Update 01_Basics_Of_Numpy.py

--- 01_NUMPY/01_Basics_Of_Numpy.py
+++ 01_NUMPY/01_Basics_Of_Numpy.py
@@ -1,1 +1,33 @@
-unik
+# ----- ----- NumPy ----- -----
+
+# - NumPy : Numerical Python
+# - It is used to do Mathematical Operations.
+
+# NumPy is a library used for:
+
+# - Numerical operations
+# - Fast array processing
+# - Mathematical computations
+# - It is faster than Python lists because:
+#       :- Uses contiguous memory
+#       :- Written in C
+
+#==========================================
+
+# Difference Between List and Array :
+
+# List : It uses Non-Continious Memory Allocation.
+# Array : It uses Continious Memory Allocation.
+
+# List : Less Memory Wastage.
+# Array : More Memory Wastage.
+
+# List : It can Store Different Kind of Data.
+# Array : It can Store Similar Kind of Data.
+
+# List : [1,2,3]+[4,5,6] --> [1,2,3,4,5,6]
+# Array : [1,2,3]+[4,5,6] --> [5,7,9]
+
+#==========================================
+
+
