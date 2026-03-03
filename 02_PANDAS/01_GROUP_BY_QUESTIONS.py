b7f97c1043f13bf96a2b7c8fc2be87f9c6ca74f7
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2026-03-03 10:40:24 +0530

    Update 01_GROUP_BY_QUESTIONS.py

--- 02_PANDAS/01_GROUP_BY_QUESTIONS.py
+++ 02_PANDAS/01_GROUP_BY_QUESTIONS.py
@@ -9,8 +9,26 @@ df.groupby('Category')['Quantity'].mean()
 
 
 # 3) Find both total sales AND average quantity per Category in a single query.
-
 df.groupby('Category').agg({
     'Total_Sales': 'sum',
     'Quantity': 'mean'
 })
+
+
+# 4) Find the total sales per City.
+df.groupby('City')['Total_Sales'].sum()
+
+
+# 5) Find the total sales per City and Category combination.
+
+df.groupby(['City','Category'])['Total_Sales'].sum()
+# Whenever we want to pass multiple columns, that time we need to inclose in a SQUARE BRACKETS [].
+
+
+# 6) Find the top-selling Category overall (based on total sales).
+df.groupby('Category')['Total_Sales'].sum().sort_values(ascending=False).head(1) 
+
+
+# 7) Find the top-selling Category within each City.
+
+df.groupby(['City', 'Category'])['Total_Sales'].sum().groupby(level=0).idxmax()
