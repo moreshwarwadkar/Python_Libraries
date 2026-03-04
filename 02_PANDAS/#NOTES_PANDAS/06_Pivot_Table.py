c1846a4499c908fa9556adbf15179974bc999b8f
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2026-03-04 20:59:19 +0530

    Create 06_Pivot_Table.py

---
+++ 02_PANDAS/#NOTES_PANDAS/06_Pivot_Table.py
@@ -0,0 +1,39 @@
+# ----- ----- PIVOT_TABLE ----- -----
+
+# - It is used to perform multiple aggregation functionon multiple columns at same time.
+# - It is also known as Powerhous of Pandas.
+
+# - Syntax : pf.pivot_table(values=['COL1','COL2',...,'COLn'],index='CATEGORICAL COLUMN',aggfunc=['FunName1','FName2'...,'FNameN'])
+
+
+# SAMPLE DATASSET (USE FOR FOLLOWING EXAMPLES) : 
+
+import pandas as pd
+
+data = {
+    "City":["Mumbai","Mumbai","Pune","Pune","Delhi","Delhi"],
+    "Category":["Electronics","Clothing","Electronics","Clothing","Electronics","Clothing"],
+    "Sales":[5000,2000,3000,1500,4000,2500]
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+# OUTPUT : 
+'''
+City	   Category	      Sales
+Mumbai	 Electronics	  5000
+Mumbai	 Clothing	      2000
+Pune	   Electronics	  3000
+Pune	   Clothing	      1500
+Delhi	   Electronics	  4000
+Delhi	   Clothing	      2500
+'''
+
+# EXAMPLE 1 : TOTAL SALES PER CITY 
+df.pivot_table(values='Sales', index='City', aggfunc='sum')
+
+# OUTPUT :
+'''
+
+'''
